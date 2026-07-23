3cdb1077ff6a66fb3b7ca974977d2bffbbcc728d
Daksh-Dixit21 <dixitdaksh21@gmail.com>
2026-07-23 21:40:03 +0530

    Attempt to fix render error

--- backend/database.py
+++ backend/database.py
@@ -9,14 +9,7 @@ load_dotenv(ROOT_DIR / '.env')
 MONGO_URL = os.environ.get('MONGO_URL', 'mongodb://localhost:27017')
 DB_NAME = os.environ.get('DB_NAME', 'vidyaloop')
 
-client = AsyncIOMotorClient(
-    MONGO_URL,
-    tls=True,
-    tlsAllowInvalidCertificates=False,
-    serverSelectionTimeoutMS=30000,
-    retryWrites=True,
-    retryReads=True,
-)
+client = AsyncIOMotorClient(MONGO_URL)
 db = client[DB_NAME]
 
 users_collection = db['users']
